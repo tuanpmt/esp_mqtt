8f0fa38c7e9aac3d0a3d6423fedf4fd8f57d62d0
EAD Fritz <ead.fritz@gmail.com>
2015-01-08 21:12:10 +0100

    moved a #include "user_interface.h" from user_interface.h to where it acually was needed

--- include/user_config.h
+++ include/user_config.h
@@ -1,6 +1,5 @@
 #ifndef _USER_CONFIG_H_
 #define _USER_CONFIG_H_
-#include "user_interface.h"
 
 #define CFG_HOLDER	0x00FF55A4
 #define CFG_LOCATION	0x3C	/* Please don't change or if you know what you doing */
