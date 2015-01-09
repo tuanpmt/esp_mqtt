372dc409db0a7959cbca1f3bbddea650f4ee451c
Tuan PM <tuanpm@live.com>
2015-01-09 08:13:22 +0700

    need CLIENT_SSL_ENABLE for libssl work

--- include/user_config.h
+++ include/user_config.h
@@ -21,7 +21,7 @@
 
 #define MQTT_RECONNECT_TIMEOUT 	5	/*second*/
 
-#define MQTT_CLIENT_SSL_ENABLE
-//#undef MQTT_CLIENT_SSL_ENABLE
+//#define CLIENT_SSL_ENABLE
+
 
 #endif
--- user/config.c
+++ user/config.c
@@ -94,7 +94,7 @@ void CFG_Load()
 		sysCfg.mqtt_port = MQTT_PORT;
 		os_sprintf(sysCfg.mqtt_user, "%s", MQTT_USER);
 		os_sprintf(sysCfg.mqtt_pass, "%s", MQTT_PASS);
-#ifdef MQTT_CLIENT_SSL_ENABLE
+#ifdef CLIENT_SSL_ENABLE
 		sysCfg.security = SEC_SSL;
 #else
 		sysCfg.security = SEC_NONSSL;
