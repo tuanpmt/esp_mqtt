d69d24da4ac0c3c3dbc28f3698834fd80be989d3
EAD Fritz <ead.fritz@gmail.com>
2015-01-18 12:24:39 +0100

    moved CLIENT_SSL_ENABLE away from the 'default configurations' section

--- include/user_config.h
+++ include/user_config.h
@@ -3,6 +3,7 @@
 
 #define CFG_HOLDER	0x00FF55A4	/* Change this value to load default configurations */
 #define CFG_LOCATION	0x3C	/* Please don't change or if you know what you doing */
+#define CLIENT_SSL_ENABLE
 
 /*DEFAULT CONFIGURATIONS*/
 
@@ -15,17 +16,13 @@
 #define MQTT_USER			"DVES_USER"
 #define MQTT_PASS			"DVES_PASS"
 
-
 #define STA_SSID "DVES_HOME"
 #define STA_PASS "yourpassword"
 #define STA_TYPE AUTH_WPA2_PSK
 
 #define MQTT_RECONNECT_TIMEOUT 	5	/*second*/
 
-#define CLIENT_SSL_ENABLE
-
 #define DEFAULT_SECURITY	0
-
 #define QUEUE_BUFFER_SIZE		 		2048
 
 #endif
